ac73d3df63e103c940934dfae3254b5b30e631ca
ada1ra <adada0901@gmail.com>
2025-11-07 23:28:32 +0300

    Add bubble sort

---
+++ src/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    if not arr:
+        return arr.copy()
+    result = arr.copy()
+    n = len(result)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if result[j] > result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+    return result
